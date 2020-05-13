463b046a103bc339db3dda10014f21918320ac4f
InsideU <umang20singh@gmail.com>
2020-05-13 22:43:17 +0530

    status changed

--- templates/skeleton.cpp
+++ templates/skeleton.cpp
@@ -2,37 +2,34 @@
  
 using namespace std;
  
-	#define ll 				int64_t
+	#define ll 			int64_t
 	#define sbits 			__builtin_popcount
 	#define umap 			unordered_map<ll,ll> umap
-	#define fill(vect,n) 	fill(vect.begin(),vect.end(),n) 
-	#define pb 				push_back
-	#define pf 				push_front
+	#define fill(vect,n) 		fill(vect.begin(),vect.end(),n) 
+	#define pb 			push_back
+	#define pf 			push_front
 	#define F(n) 			for(ll i=0;i<n;i++) 
 	#define loop(i,a,b)		for(ll i=(ll)a;i<(ll)b;++i)
-	#define rloop(i,a,b)    for(ll i=(ll)a;i<=(ll)b;++i)
+	#define rloop(i,a,b)    	for(ll i=(ll)a;i<=(ll)b;++i)
 	#define fastIO 			ios_base::sync_with_stdio(false);cin.tie(0)
 	#define vect(n) 		vector <ll> vect(n)
 	#define w(x) 			int x; cin >> x; while(x--)
 	#define mod 			10e9+7
 	#define INF 			numeric_limits<ll>::max();
 	
-vector<ll> adj[1000001];
-bool visited[1000001];
-	template<typename T>
-	void dfs(T src){
-			visited[src]=true;
-			for(auto neighbour:adj[src]){
-					if(!visited[neighbour]) dfs(neighbour);
-			}
-		}
  
  
 int main(){
 	fastIO;
 	w(x){
-		cout << "Hello";
-		cout << INF;
-return 0;
+	
+
+
+
+
+
+	
 	}
+
+return 0;
 }
